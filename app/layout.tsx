36d2e3c0e3ba67dc557d4d523ad5c2e53edd9430
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-11-04 18:06:31 +0600

    Basic Version Implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
-  title: "EventSphere",
-  description: "EventSphere is a platform for creating and managing events.",
+  title: 'Evently',
+  description: 'Evently is a platform for event management.',
   icons: {
-    icon: "/favicon.ico",
-  },
-};
+    icon: '/assets/images/logo.svg'
+  }
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
